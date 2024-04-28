cd61de7243ae7618c55fa0f3286d4eced5ffa5f2
YuFireWhisper <129593181+YuFireWhisper@users.noreply.github.com>
2024-04-28 18:13:03 +0800

    修哲提交

--- main.py
+++ main.py
@@ -4,19 +4,39 @@ import time
 import pygame
 
 
+# 設置視窗大小
+WIDTH, HEIGHT = 1180 * 3, 548 * 3
+
+# 設定常用RGB
+BLACK = (0, 0, 0)
+
+
+class Player(pygame.sprite.Sprite):
+    """玩家角色"""
+
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("img/mini_man.png")
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        
+    def move_left(self):
+        """向左移動"""
+        
+
+
 def main() -> None:
     """主程式"""
     pygame.init()
     pygame.mixer.init()
-    # 設置視窗大小
-    WIDTH, HEIGHT = 1180 * 3, 548 * 3
+    pygame.mixer.Sound("sound/get_gold_coins.wav").play()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("receve glod coins game")
     # 引入圖片
     board = pygame.image.load("img/board.png")
     board = pygame.transform.scale(board, (1180 * 3, 548 * 3))
     coin = pygame.image.load("img/coin.png")
-    mini_man = pygame.image.load("img/mini_man.png")
+    mini_man = Player()
     screen.blit(board, (0, 0))
     running = True
     pygame.display.update()
