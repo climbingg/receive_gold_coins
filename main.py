ce784fbafeaad45a5b49647569dad3d12b09dc22
climbingg <xuezhiqian759@gmail.com>
2024-05-02 07:08:45 +0800

    裁切mini_man.png 增加到變成能夠玩一個基礎20秒小遊戲，最後顯示final score

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 """接金幣遊戲專案"""
 
+import random
 import time
 import os
 import pygame
 
 
 # 初始化
+pygame.init()
+pygame.mixer.init()
 all_sprites = pygame.sprite.Group()
 
 # 設定幀數
@@ -16,6 +19,13 @@ WIDTH, HEIGHT = 1180 * 3, 548 * 3
 
 # 設定常用RGB
 BLACK = (0, 0, 0)
+
+# 分數初始化
+score = [0]
+
+# 設定字體
+font = pygame.font.SysFont("Arial", 108)
+final_font = pygame.font.SysFont("Arial", 600)
 
 
 class Player(pygame.sprite.Sprite):
@@ -48,7 +58,9 @@ class Player(pygame.sprite.Sprite):
             self.move_left()
         if keys[pygame.K_RIGHT]:
             self.move_right()
-        
+
+
+mini_man = Player()
 
 
 class Coin(pygame.sprite.Sprite):
@@ -56,14 +68,24 @@ class Coin(pygame.sprite.Sprite):
 
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("img/coin.png")
+        self.image = pygame.transform.scale(pygame.image.load("img/coin_1.png"), (100, 100))
+        self.speed = random.randint(10, 20)
         self.rect = self.image.get_rect()
+        self.rect.centerx = random.randint(200, 1180 * 3 - 200)
+        self.rect.centery = 200
+
+    def update(self) -> None:
+        """更新畫面"""
+        self.rect.y += self.speed
+        if self.rect.bottom > HEIGHT:
+            self.kill()
+        if pygame.sprite.spritecollide(mini_man, [self], False):
+            score[0] += 100
+            self.kill()
 
 
 def main() -> None:
     """主程式"""
-    pygame.init()
-    pygame.mixer.init()
     pygame.mixer.music.load(os.path.join("sound", "background.ogg"))
     pygame.mixer.music.play(-1)
     get_coins_sound = pygame.mixer.Sound("sound/get_gold_coins.wav")
@@ -73,19 +95,32 @@ def main() -> None:
     # 引入圖片
     board = pygame.image.load("img/board.png")
     board = pygame.transform.scale(board, (1180 * 3, 548 * 3))
-    mini_man = Player()
     all_sprites.add(mini_man)
     running = True
     pygame.display.update()
+    t = time.time()
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        if time.time() - t > 20:
+            running = False
+            all_sprites.empty()
+            rendered_text = final_font.render(f"final score: {score[0]}", True, BLACK)
+            text_rect = rendered_text.get_rect(center=(WIDTH / 2, HEIGHT / 2))
+            screen.blit(rendered_text, text_rect)
+            pygame.display.update()
+            time.sleep(3)
         clock.tick(FPS)
         screen.fill(BLACK)
+        if random.randint(1, 15) == 1:
+            all_sprites.add(Coin())
         all_sprites.update()
         screen.blit(board, (0, 0))
         all_sprites.draw(screen)
+        rendered_text = font.render(f"current score: {score[0]}", True, BLACK)
+        text_rect = rendered_text.get_rect(center=(3000, 200))
+        screen.blit(rendered_text, text_rect)
         pygame.display.update()
     pygame.quit()
 
